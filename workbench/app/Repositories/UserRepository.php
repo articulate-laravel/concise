3d5b760e4b9f9a36eff3c4338a0cea9f1fe76a91
Ollie Read <code@ollieread.com>
2025-02-18 00:31:45 +0000

    chore: Add repository hook functionality for Laravel auth

--- workbench/app/Repositories/UserRepository.php
+++ workbench/app/Repositories/UserRepository.php
@@ -3,29 +3,16 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Entities\User;
+use Articulate\Concise\Concerns\SupportsAuth;
 use Articulate\Concise\Concerns\UsesTransactions;
+use Articulate\Concise\Contracts\AuthRepository;
 use Articulate\Concise\Support\BaseRepository;
-use SensitiveParameter;
 
 /**
  * @extends \Articulate\Concise\Support\BaseRepository<\App\Entities\User>
  */
-final class UserRepository extends BaseRepository
+final class UserRepository extends BaseRepository implements AuthRepository
 {
-    use UsesTransactions;
-
-    public function findById(int $id): ?User
-    {
-        return $this->hydrate(
-            $this->query()->where('id', $id)->first()
-        );
-    }
-
-    public function findByToken(#[SensitiveParameter] string $token): ?User
-    {
-        return $this->hydrate(
-            $this->query()->where('remember_token', $token)->first()
-        );
-    }
+    use UsesTransactions,
+        SupportsAuth;
 }
